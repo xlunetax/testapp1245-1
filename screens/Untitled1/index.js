78215907c5f925c9abfc2553e1ae7482d16db80b
aline+5@crowdbotics.com <aline+5@crowdbotics.com>
2022-07-26 15:08:00 -0400

    Crowdbotics Studio: Update app screens

--- screens/Untitled1/index.js
+++ screens/Untitled1/index.js
@@ -9,8 +9,8 @@ const Untitled1 = () => {
     height: '100%'
   }}>
       <View style={{
-      left: 58,
-      top: 79,
+      left: 17,
+      top: 54,
       position: "absolute",
       height: 60,
       width: 140,
